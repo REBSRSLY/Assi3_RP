0e26c7ec1903768c37cbdb237cd6c7ee757b9d3d
REBSRSLY <150343232+REBSRSLY@users.noreply.github.com>
2026-04-29 18:08:39 +0200

    Add Streamlit integration with Google Sheets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
+# Creazione della connessione
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# Lettura dei dati (usa il nome del foglio se necessario)
+df = conn.read(spreadsheet="Assi3RP")
+
+st.write("Ecco i dati dal mio Google Sheet:")
+st.dataframe(df)
